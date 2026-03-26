3d6b7d68d4e23f6097d6d2292e7ec2c57d74a48f
gunjanwebdesign-svg <gunjan.webdesign@gmail.com>
2026-03-26 07:53:53 +0530

    Update ServicesSection.tsx

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -198,7 +198,7 @@ const ServicesSection = () => {
               className="max-w-none max-h-none"
               style={{
                 display: "block",
-                margin: "auto".
+               
               }}
             />
           </div>
